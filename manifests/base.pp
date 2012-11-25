55964306247dc4582f3708aea8614a2f1bd27ee2
mh <mh@immerda.ch>
2012-11-25 20:05:41 +0100

    add EL6 logrotate file

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,7 +5,7 @@ class logrotate::base {
 
   file{"/etc/cron.daily/logrotate":
     source => [ "puppet:///modules/site_logrotate/${::fqdn}/logrotate.cron.daily",
-                "puppet:///modules/logrotate/logrotate.cron.daily.${::operatingsystem}.${::lsbdistcodename}",
+                "puppet:///modules/logrotate/logrotate.cron.daily.${::operatingsystem}.${::lsbmajdistrelease}",
                 "puppet:///modules/logrotate/logrotate.cron.daily.${::operatingsystem}",
                 "puppet:///modules/logrotate/logrotate.cron.daily" ],
     require => Package[logrotate],
