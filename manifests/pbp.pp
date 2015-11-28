c807c121e1c02b57102de825c4da0085c24cbe62
mh <mh@immerda.ch>
2015-11-28 17:41:32 +0100

    linting for future parser

--- manifests/pbp.pp
+++ manifests/pbp.pp
@@ -1,19 +1,20 @@
 # manifests/pbp.pp
 # manages our logrotate setup
-
 class logrotate::pbp inherits logrotate::base {
   # fix that for our tmp_tmp hack
   if $::tmp_device {
-    file{"/var/roottmp":
+    file{'/var/roottmp':
       ensure => directory,
-      mode => 0750, owner => root, group => 0;
+      owner  => root,
+      group  => 0,
+      mode   => '0750';
     }
 
-    File["/etc/cron.daily/logrotate"]{
+    File['/etc/cron.daily/logrotate']{
       source => [ "puppet:///modules/logrotate/tmptmp/logrotate.cron.daily.${::operatingsystem}.${::operatingsystemmajrelease}",
                   "puppet:///modules/logrotate/tmptmp/logrotate.cron.daily.${::operatingsystem}",
-                  "puppet:///modules/logrotate/tmptmp/logrotate.cron.daily" ],
-      require +> File["/var/roottmp"],
+                  'puppet:///modules/logrotate/tmptmp/logrotate.cron.daily' ],
+      require +> File['/var/roottmp'],
     }
   }
 }
